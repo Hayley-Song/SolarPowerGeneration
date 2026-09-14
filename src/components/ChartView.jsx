e760b4474db8fa1bcf0f5f5b95d76b3fd1a7653f
Hayley-Song <kawazumi2@gmail.com>
2026-09-14 11:32:35 +0900

    Update ChartView.jsx

--- src/components/ChartView.jsx
+++ src/components/ChartView.jsx
@@ -459,7 +459,7 @@ function StatCard({
   <img 
     src={cardIcon} 
     alt={title} 
-    className="w-5 h-5 mt-0.5 flex-shrink-0 opacity-70" 
+    className="w-7 h-7 mt-0.5 flex-shrink-0 opacity-70" 
   />
 
   {/* 2. 오른쪽 텍스트 & 지표 영역 */}
